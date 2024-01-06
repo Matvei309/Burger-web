ff3ab36fcfdd941d621ebb419ec7486cfff49ded
Matvey309 <mat-bazhanov02@mail.ru>
2024-01-07 01:44:22 +0300

    Исправил js, добавил 2-ю страницу(как тест), теперь все работает в разных html файлах

--- js/javascript.js
+++ js/javascript.js
@@ -1,81 +1,56 @@
-const burger = document.querySelector('.burger');
-const main = document.querySelectorAll('.main');
-const shadow = document.querySelectorAll('.shadow');
-const linkList = document.querySelector('.link__list');
-const items = document.querySelectorAll('.link__list-link');
-const screens = document.querySelectorAll('.screen');
-const text = document.querySelectorAll('.next-page');
-const container  = document.querySelector('.container')
-let index = true;
-const listWeb = ['Home', 'Winter', 'Forest', 'Beach'];
+function EfectFunction() {
+  const shadow = document.querySelectorAll('.shadow');
+  const main = document.querySelector('.main');
+  const nextPage = document.querySelector(".next-page")
 
-function clickElement() {
-  burger.classList.toggle('activeBurger');
-  main.forEach(e => {
-    e.classList.toggle('activeMain')
-  })
+  main.classList.toggle('activeMain')
   shadow[0].classList.toggle('activeShadowOne');
   shadow[1].classList.toggle('activeShadowTwo');
-  linkList.classList.toggle('listActive')
-  items.forEach(element => {
-    element.classList.toggle('linkActive');
-  })
+  nextPage.style.display = 'none'
 
-  main.forEach(m => {
-    m.addEventListener('mouseenter', () => {
-      m.classList.toggle('hoverMain')
-      shadow[0].classList.toggle('hoverShadow')
-    })
+}
 
-    m.addEventListener('mouseleave', () => {
-      m.classList.remove('hoverMain')
-      shadow[0].classList.remove('hoverShadow')
-    })
+
+function findHtmlFile(dataLink) {
+  const pageLink = [
+    { Home: "../html/first_page.html"},
+    { Winter: "../html/second_page.html"}
+  ]
+  pageLink.forEach(item => {
+    if (dataLink == Object.keys(item)) {
+      setTimeout(() => {
+        window.location.href = Object.values(item)
+      }, 1 * 1000)
+    }
   })
 }
 
-function replaceBg(id) {
-  const bg = document.getElementById(id)
-  screens.forEach(element => {
-    element.style.display = 'none';
-  })
-  bg.style.display = 'block';
-}
+function changeHtmlFile() {
+  const linkItems = document.querySelectorAll('.link__list-link');
+  const main = document.querySelector('.main');
 
-function changeBg() {
-  items.forEach(link => {
-    link.addEventListener('mouseenter', (e) => {
-      e.preventDefault()
-      replaceBg(e.target.dataset.link)
-    })
 
+  linkItems.forEach(link => {
+    link.classList.toggle('linkActive')
     link.addEventListener('click', e => {
       e.preventDefault();
-      localStorage.setItem('web', JSON.stringify(e.target.dataset.link))
-      text.forEach(element => {
-        element.style.display = 'block'
-      })
-      clickElement();
-      location.reload()
-    })
-  })
-  screens.forEach(screen => {
-    screen.style.display = 'none';
-    listWeb.forEach((e, index) => {
-      if (e === JSON.parse(localStorage.getItem('web'))) {
-        screens[index].style.display = 'block';
-      }
+      main.classList.toggle('mainLeave')
+      findHtmlFile(link.dataset.link)
+      EfectFunction();
     })
   })
 }
 
-burger.addEventListener('click', (e) => {
-  text.forEach(element => {
-    element.style.display = 'none';
+function clickOnBurger() {
+  const burger = document.querySelector('.burger');
+
+
+  burger.addEventListener('click', (e) => {
+    e.preventDefault()
+    burger.classList.toggle('activeBurger');
+    EfectFunction()
   })
-  clickElement()
-  }
-)
+}
 
-
-changeBg()
+clickOnBurger()
+changeHtmlFile()
